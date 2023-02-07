58d09a5241416de38d1a90aad1810d18173f3906
rpXcmd <54814103+rpXcmd@users.noreply.github.com>
2023-02-06 23:32:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# to new beginnings
+#Python 3 script to save log file and process HTTP request counts
+#requires 'sudo apt install python3-pip' to be run first
+#requires 'pip install requests' afterwards
+
+import requests
+from datetime import datetime
+
+url = "https://s3.amazonaws.com/tcmg476/http_access_log"
